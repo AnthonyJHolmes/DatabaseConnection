a5130793c01cef0f66cc1f3e609e07bb1c1df919
ajh3 <ajh3@CSG25-07.cs.ucc.ie>
2016-01-19 17:09:07 +0000

    Initial Database connection

---
+++ src/databaseconnection/DatabaseConnection.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databaseconnection;
+
+/**
+ *
+ * @author ajh3
+ */
+import java.sql.*;  // for standard JDBC programs
+import java.math.*; // for BigDecimal and BigInteger support
+
+public class DatabaseConnection {
+
+    public void insert() {
+    }
+
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://cs1.ucc.ie:3306/";
+        String dbName = "2016_ajh3";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "ajh3";
+        String password = "";
+
+        try {
+            Class.forName(driver).newInstance();
+            Connection conn = DriverManager.getConnection(url + dbName, userName, password);
+            Statement st = conn.createStatement();
+            ResultSet res = st.executeQuery("SELECT * FROM  WinLoss");
+            while (res.next()) {
+                int id = res.getInt("UserID");
+                int gamesPlayed = res.getInt("GamesPlayed");
+                int wins = res.getInt("Win");
+                int losses = res.getInt("Loss");
+
+                System.out.println(id + "\t" + gamesPlayed + "\t" + wins + "\t" + losses);
+            }
+//                       
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
